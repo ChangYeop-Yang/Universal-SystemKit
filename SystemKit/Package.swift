691e750df4c1b9ded80018f3cff816612e5f22b4
ChangYeop-Yang <ChangYeop-Yang@users.noreply.github.com>
2022-10-07 00:24:06 +0900

    Update

--- SystemKit/Package.swift
+++ SystemKit/Package.swift
@@ -38,23 +38,34 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: namePackage, targets: ["SystemKit"]),
-        .library(name: "Beltex", targets: ["Beltex"])
+        .library(name: FrameworkInfo.Beltex.name, targets: [FrameworkInfo.Beltex.name])
     ],
     dependencies: dependenciesPackage,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: namePackage, dependencies: dependenciesTarget),
-        .binaryTarget(name: "Beltex", path: "Frameworks/Beltex.xcframework"),
+        .binaryTarget(name: FrameworkInfo.Beltex.name, path: FrameworkInfo.Beltex.path),
         .testTarget(name: "SystemKitTests", dependencies: ["SystemKit", "Beltex"])
     ]
 )
 
 // MARK: - Enum
-public enum PackageAttribute: String, CaseIterable {
+public enum FrameworkInfo: String, CaseIterable {
     
-    /// [Apple SwiftLog OpenSource](https://github.com/apple/swift-log)
-    case Logging = "Logging"
+    case Beltex = "Beltex"
+    
+    public var path: String {
+        switch self {
+        case .Beltex:
+            return "Frameworks/Beltex.xcframework"
+        }
+    }
+    
+    public var name: String { return self.rawValue }
+}
+
+public enum PackageAttribute: String, CaseIterable {
     
     /// [Apple Collections OpenSource](https://github.com/apple/swift-collections)
     case Collections = "Collections"
@@ -66,8 +77,6 @@ public enum PackageAttribute: String, CaseIterable {
         switch self {
         case .Collections:
             return "https://github.com/apple/swift-collections.git"
-        case .Logging:
-            return "https://github.com/apple/swift-log.git"
         case .SystemPackage:
             return "https://github.com/apple/swift-system.git"
         }
@@ -75,8 +84,6 @@ public enum PackageAttribute: String, CaseIterable {
     
     public var packageName: String {
         switch self {
-        case .Logging:
-            return "swift-log"
         case .Collections:
             return "swift-collections"
         case .SystemPackage:
@@ -86,9 +93,6 @@ public enum PackageAttribute: String, CaseIterable {
     
     public var upToNextMajor: Range<Version> {
         switch self {
-        case .Logging:
-            let fromVersion = Version(stringLiteral: "1.0.0")
-            return .upToNextMajor(from: fromVersion)
         case .Collections:
             let fromVersion = Version(stringLiteral: "1.0.3")
             return .upToNextMajor(from: fromVersion)
