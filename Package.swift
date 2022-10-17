39190b93e836254b5a562c81db98ba35db563bc5
ChangYeop-Yang <ChangYeop-Yang@users.noreply.github.com>
2022-10-17 22:49:09 +0900

    [UPDATE] 2022.10.17. 22:48
    [UPDATE] 2022.10.17. 22:48 SKSignal Guide 페이지 작업

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /*
- * Copyright (c) 2022 ChangYeop-Yang. All rights reserved.
+ * Copyright (c) 2022 Universal SystemKit. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
