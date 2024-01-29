bf79d4c1a550f3289b29055cd9e21f2bce62d0b5
ChangYeop-Yang <ChangYeop-Yang@users.noreply.github.com>
2024-01-29 22:09:52 +0900

    Update 2024.01.29.
    Objective-C 프레임워크 오류 사항 수정 및 HTTP 오픈소스 추가

--- Package.swift
+++ Package.swift
@@ -32,33 +32,39 @@ let package = Package(
     name: InfoPackage.PackageName,
     // The list of minimum versions for platforms supported by the package.
     platforms: [
-        // macOS 10.13 (High Sierra) 이상의 운영체제부터 사용이 가능합니다.
-        .macOS(SupportedPlatform.MacOSVersion.v10_13),
+        // macOS 10.15 (Catalina) 이상의 운영체제부터 사용이 가능합니다.
+        .macOS(SupportedPlatform.MacOSVersion.v10_15),
         
-        // iOS 11 이상의 운영체제부터 사용이 가능합니다.
-        .iOS(SupportedPlatform.IOSVersion.v11),
+        // iOS 13 이상의 운영체제부터 사용이 가능합니다.
+        .iOS(SupportedPlatform.IOSVersion.v13),
     ],
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     products: [
+        // It is a module library related to Swift source code.
         .library(name: InfoPackage.PackageName, targets: [InfoPackage.PackageName]),
+        
+        // It is a module library related to Objective-C source code.
+        .library(name: InfoPackage.PackageObjcName, targets: [InfoPackage.PackageObjcName])
     ],
     // The list of package dependencies.
     dependencies: [
         .package(url: RemotePackage.SwiftLog.path, from: RemotePackage.SwiftLog.from),
+        .package(url: RemotePackage.SwiftHTTPTypes.path, from: RemotePackage.SwiftHTTPTypes.from),
     ],
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     targets: [
         .binaryTarget(name: LocalPackage.PLCrashReporter.name, path: LocalPackage.PLCrashReporter.path),
         .target(name: InfoPackage.PackageName,
-                dependencies: [LocalPackage.PLCrashReporter.target, RemotePackage.SwiftLog.target],
-                path: InfoPackage.PackagePath,
+                dependencies: [LocalPackage.PLCrashReporter.target, RemotePackage.SwiftLog.target, RemotePackage.SwiftHTTPTypes.target],
+                path: InfoPackage.PackagePath),
+        .target(name: InfoPackage.PackageObjcName, 
+                path: InfoPackage.PackageObjcPath, 
                 sources: ["Cryptor/SKSecurity.mm"],
                 publicHeadersPath: ".",
                 cSettings: [
                     .headerSearchPath(".")
-                ]
-        ),
+                ])
     ],
     // The list of Swift versions with which this package is compatible.
     swiftLanguageVersions: [.v5]
@@ -78,6 +84,10 @@ public struct InfoPackage {
     public static let PackageName: String = "SystemKit"
     
     public static let PackagePath: String = "Sources"
+    
+    public static let PackageObjcName: String = "SystemKit_Objc"
+    
+    public static let PackageObjcPath: String = "ObjcSources"
     
     public static let platforms: [PackageDescription.Platform] = [.iOS, .macOS]
 }
@@ -116,33 +126,44 @@ public enum LocalPackage: String, CaseIterable, PackageProtocol {
 
 public enum RemotePackage: String, CaseIterable, PackageProtocol {
     
-    /// [swift-docc-plugin - GitHub](https://github.com/apple/swift-docc-plugin)
+    /// 🌎 [Swift-docc-plugin - GitHub](https://github.com/apple/swift-docc-plugin)
     case SwiftDocC = "SwiftDocCPlugin"
     
-    /// [SwiftLog - GitHub](https://github.com/apple/swift-log)
+    /// 🌎 [SwiftLog - GitHub](https://github.com/apple/swift-log)
     case SwiftLog = "swift-log"
     
+    /// 🌎 [Swift-HTTP-Types- GitHub](https://github.com/apple/swift-http-types)
+    case SwiftHTTPTypes = "swift-http-types"
+    
     public var path: String {
+        
         switch self {
         case .SwiftDocC:
             return "https://github.com/apple/swift-docc-plugin.git"
         case .SwiftLog:
             return "https://github.com/apple/swift-log.git"
+        case .SwiftHTTPTypes:
+            return "https://github.com/apple/swift-http-types.git"
         }
     }
     
     public var from: Version {
+        
         switch self {
         case .SwiftDocC:
             // https://github.com/apple/swift-docc-plugin/releases/tag/1.2.0
             return Version(1, 2, 0)
         case .SwiftLog:
-            // https://github.com/apple/swift-log/releases/tag/1.5.2
-            return Version(1, 5, 2)
+            // https://github.com/apple/swift-log/releases/tag/1.5.4
+            return Version(1, 5, 4)
+        case .SwiftHTTPTypes:
+            // https://github.com/apple/swift-http-types/releases/tag/1.0.3
+            return Version(1, 0, 3)
         }
     }
     
     public var target: Target.Dependency {
+        
         switch self {
         case .SwiftDocC:
             let condition = TargetDependencyCondition.when(platforms: InfoPackage.platforms)
@@ -150,6 +171,9 @@ public enum RemotePackage: String, CaseIterable, PackageProtocol {
         case .SwiftLog:
             let condition = TargetDependencyCondition.when(platforms: InfoPackage.platforms)
             return .product(name: "Logging", package: self.name, condition: condition)
+        case .SwiftHTTPTypes:
+            let condition = TargetDependencyCondition.when(platforms: InfoPackage.platforms)
+            return .product(name: "HTTPTypes", package: self.name, condition: condition)
         }
     }
     
